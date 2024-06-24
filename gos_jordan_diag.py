ef8a63fcb130cd0cd36c848dea6c03a262569e11
Ehsan <ehsanmelm82@gmail.com>
2024-06-24 19:32:31 +0330

    style : delete unused comment

--- gos_jordan_diag.py
+++ gos_jordan_diag.py
@@ -1,11 +1,5 @@
-# def evaluate_diag(matrix , rows , cols):
-#     diag = 0
-#     for row in range(rows):
-#         for col in range(col):
-#             if row == col:
-                
-
 from copy import deepcopy
+
 def GosJordan_diag(matrix):
     copy_matrix = deepcopy(matrix)
 
@@ -17,7 +11,6 @@ def GosJordan_diag(matrix):
 
         for j in range(i+1 , row):
 
-            # if abs(copy_matrix[j , i])> abs(copy_matrix[main_element,i]):
             if abs(copy_matrix[j][i])> abs(copy_matrix[main_row][i]):
                 main_row = j
 
@@ -31,19 +24,20 @@ def GosJordan_diag(matrix):
         
         main_element = copy_matrix[i][i]
         if main_element != 0 :
+
             for j in range((len(copy_matrix[i]))):
                 copy_matrix[i][j] //= float(main_element)
-                # copy_matrix[i][j] /= main_element
 
         for j in range(row):
+
             if j != i:
+
                 factor = copy_matrix[j][i] / copy_matrix[i][i] if copy_matrix[i][i] != 0 else 0
-                # A[j, :] -= factor * A[i, :]
+
                 for k in range(len(copy_matrix[j])):
                     copy_matrix[j][k] = copy_matrix[j][k] - (factor * copy_matrix[i][k])
         
-    # evaluate_diag(copy_matrix , row , col)
     print(copy_matrix)
-            # copy_matrix[]
+
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 dig = GosJordan_diag(matrix)
