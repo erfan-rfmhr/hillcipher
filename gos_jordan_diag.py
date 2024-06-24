94cad322662f425cc101be352a782cada038b4f7
Ehsan <ehsanmelm82@gmail.com>
2024-06-24 19:20:30 +0330

    feat : add gos-jordan function

---
+++ gos_jordan_diag.py
@@ -0,0 +1,12 @@
+# def evaluate_diag(matrix , rows , cols):
+#     diag = 0
+#     for row in range(rows):
+#         for col in range(col):
+#             if row == col:
+                
+
+from copy import deepcopy
+def GosJordan_diag(matrix):
+    pass
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+dig = GosJordan_diag(matrix)
